27103665830ee5f8076775b846db4b911a6a8946
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2016-01-02 14:10:03 +0000

    Initial Commit

---
+++ src/net/mjhutchinson/nodegrapheditor/Main.java
@@ -0,0 +1,27 @@
+package net.mjhutchinson.nodegrapheditor;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Michael Hutchinson on 02/01/2016 at 14:05.
+ */
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("NodeEditor");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/net/mjhutchinson/nodegrapheditor/MainWindowController.java
@@ -0,0 +1,7 @@
+package net.mjhutchinson.nodegrapheditor;
+
+/**
+ * Created by Michael Hutchinson on 02/01/2016 at 14:05.
+ */
+public class MainWindowController {
+}
